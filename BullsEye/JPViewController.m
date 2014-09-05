9a6041e3c2e747fa828a3996bd726c1dd629130e
Josh <jpetrack@hmc.edu>
2014-09-05 00:09:33 -0700

    Added random numbers (committed slightly late)

--- BullsEye/JPViewController.m
+++ BullsEye/JPViewController.m
@@ -15,12 +15,16 @@
 @implementation JPViewController
 {
     int _currentValue;
+    int _targetValue;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self startNewRound]; }
+
+- (void)startNewRound {
+    _targetValue = 1 + arc4random_uniform(100); _currentValue = 50;
+    self.slider.value = _currentValue;
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,9 +33,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)showAlert {
-    NSString *message = [NSString stringWithFormat:
-        @"The value of the slider is: %d", _currentValue];
+- (IBAction)showAlert
+    {NSString *message = [NSString stringWithFormat:
+        @"The value of the slider is: %d\nThe target value is: %d",
+        _currentValue, _targetValue];
     UIAlertView *alertView = [[UIAlertView alloc]
         initWithTitle:@"Hello, World!"
         message:message
