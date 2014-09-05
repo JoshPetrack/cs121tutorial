41259ab849b961a9897067f7e6c74306def36e1a
Josh <jpetrack@hmc.edu>
2014-09-04 20:48:00 -0700

    Initial Commit

---
+++ BullsEye/JPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPAppDelegate.h
+//  BullsEye
+//
+//  Created by Josh Petrack on 9/4/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BullsEye/JPViewController.h
@@ -0,0 +1,13 @@
+//
+//  JPViewController.h
+//  BullsEye
+//
+//  Created by Josh Petrack on 9/4/14.
+//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPViewController : UIViewController
+
+@end
