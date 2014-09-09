3aa3a15d37359346a7d507f1a67f656b533cccae
Josh <jpetrack@hmc.edu>
2014-09-08 18:52:02 -0700

    Added new messages for different scores. Also added bonus points for being one off or exact.

--- BullsEye/JPViewController.m
+++ BullsEye/JPViewController.m
@@ -47,11 +47,27 @@
 - (IBAction)showAlert {
     int difference = abs(_targetValue - _currentValue);
     int points = 100 - difference;
+    
+    NSString *title;
+    if (difference == 0) {
+        title = @"Perfect!";
+        points += 100;
+    } else if (difference < 5) {
+        title = @"You almost had it!";
+        if (difference == 1) {
+            points += 50;
+        }
+    } else if (difference < 10) {
+        title = @"Pretty good!";
+    } else {
+        title = @"Not even close...";
+    }
+    
     _score += points;
     
     NSString *message = [NSString stringWithFormat:@"You scored %d points", points];
     UIAlertView *alertView = [[UIAlertView alloc]
-        initWithTitle:@"Hello, World!"
+        initWithTitle:title
         message:message
         delegate:nil
         cancelButtonTitle:@"OK"
